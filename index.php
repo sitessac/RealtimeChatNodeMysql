a7c36524690061860bbb40e84a15ff0c11958212
sitessac <86174077+sitessac@users.noreply.github.com>
2023-04-22 22:55:42 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <pre class="wp-block-syntaxhighlighter-code"><!-- include jquery and socket IO -->
+//baixar os dois a baixo e colocar na pasta js
 <script src="js/jquery.js"></script>
 <script src="js/socket.io.js"></script>
  
@@ -9,4 +10,41 @@
   var receiver = "";
   var sender = "";
  
+</script></pre>
+
+
+<pre class="wp-block-syntaxhighlighter-code"><form onsubmit="return enterName();">
+  <input id="name" placeholder="Enter name">
+  <input type="submit">
+</form>
+ 
+<ul id="users"></ul>
+     
+<script>
+    function enterName() {
+        // get username
+        var name = document.getElementById("name").value;
+ 
+        // send it to server
+        io.emit("user_connected", name);
+ 
+        // save my name in global variable
+        sender = name;
+ 
+        // prevent the form from submitting
+        return false;
+    }
+ 
+    // listen from server
+    io.on("user_connected", function (username) {
+        var html = "";
+        html += "<li><button onclick='onUserSelected(this.innerHTML);'>" + username + "</button></li>";
+ 
+        document.getElementById("users").innerHTML += html;
+    });
+ 
+    function onUserSelected(username) {
+        // save selected user in global variable
+        receiver = username;
+      }
 </script></pre>
