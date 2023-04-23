e7eefed6ead87b754613e937fa6df02cd69b7e90
sitessac <86174077+sitessac@users.noreply.github.com>
2023-04-22 22:51:09 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<pre class="wp-block-syntaxhighlighter-code"><!-- include jquery and socket IO -->
+<script src="js/jquery.js"></script>
+<script src="js/socket.io.js"></script>
+ 
+<script>
+  // creating io instance
+  var io = io("http://191.252.223.139:6000");
+ 
+  var receiver = "";
+  var sender = "";
+ 
+</script></pre>
