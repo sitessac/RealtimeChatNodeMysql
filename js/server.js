83513635d5c4d32b0a3229f115a0132fdcdbe8d7
sitessac <86174077+sitessac@users.noreply.github.com>
2023-04-22 23:14:05 -0300

    Update server.js

--- js/server.js
+++ js/server.js
@@ -43,3 +43,33 @@ socket.on("send_message", function (data) {
  
     io.to(socketId).emit("new_message", data);
 });
+
+
+// Create instance of mysql
+var mysql = require("mysql");
+ 
+// make a connection
+var connection = mysql.createConnection({
+    "host": "localhost",
+    "user": "root",
+    "password": "",
+    "database": "web_chat"
+});
+ 
+// connect
+connection.connect(function (error) {
+    // show error if any
+});
+
+// listen from client
+socket.on("send_message", function (data) {
+    // send event to receiver
+    var socketId = users[data.receiver];
+ 
+    io.to(socketId).emit("new_message", data);
+ 
+    // save in database
+    connection.query("INSERT INTO messages (sender, receiver, message) VALUES ('" + data.sender + "', '" + data.receiver + "', '" + data.message + "')", function (error, result) {
+        //
+    });
+});
